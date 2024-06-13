e039dddc9e2cd805f8d2009f7d98ce406ad3801a
minsikson <sonminsik@sonminsig-ui-MacBook-Pro.local>
2024-06-14 01:10:57 +0900

    1.4 Recap Refactor

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -4,11 +4,15 @@ const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
 
+// NOTE: 오타 방지를 위해서 type 을 별도로 정의해둠.
+const ADD = "ADD";
+const MINUS = "MINUS";
+
 const countModifier = (count = 0, action) => {
   switch (action.type) {
-    case 'ADD':
+    case ADD:
       return count + 1;
-    case 'MINUS':
+    case MINUS:
       return count - 1;
     default:
       return count;
@@ -25,10 +29,10 @@ const onChange = () => {
 countStore.subscribe(onChange);
 
 const handleAdd = () => {
-  countStore.dispatch({ type: 'ADD' });
+  countStore.dispatch({ type: ADD });
 }
 const handleMinus = () => {
-  countStore.dispatch({ type: 'MINUS' });
+  countStore.dispatch({ type: MINUS });
 }
 
 add.addEventListener("click", handleAdd);
